f92197be8bcf0453e5588c933e04300ce5fb4445
David <scenaristeur@gmail.com>
2020-04-16 16:12:26 +0200

    sw

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.c0ecd5cbf2a353c2b7c95b66a741bb77.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.b012d65d93ef9dd411b799a76411e67b.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* This work is licensed under the W3C Software and Document License
  * (http://www.w3.org/Consortium/Legal/2015/copyright-software-and-document).
