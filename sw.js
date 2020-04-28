53efe37fe14919623d59cd022e2da68d1d259a50
scenaristeur <scenaristeur@gmail.com>
2020-04-28 23:11:21 +0200

    test infinite

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.e187ea52b8f649b4577e60cda186917a.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.a1061c21451d0627b2e4310152dcb7af.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* This work is licensed under the W3C Software and Document License
  * (http://www.w3.org/Consortium/Legal/2015/copyright-software-and-document).
