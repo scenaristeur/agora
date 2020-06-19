93d6fc014ddf81bd752b59dc2f3c3352b0f34383
scenaristeur <scenaristeur@gmail.com>
2020-06-19 23:19:42 +0200

    update & fix when no message today

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.2e0725e9f17aa1f42caf85185847f0d3.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.4352bc8357fecae787fb88dcd9dd3bc3.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* This work is licensed under the W3C Software and Document License
  * (http://www.w3.org/Consortium/Legal/2015/copyright-software-and-document).
